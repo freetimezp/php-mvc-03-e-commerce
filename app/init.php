e45262d542f9efdfd8d2514a4d083202ed762bdd
freetimezp <kraftukrnet@ukr.net>
2024-11-29 20:16:15 +0200

    02. php-mvc-03-e-commerce. MVC. Folders.

---
+++ app/init.php
@@ -0,0 +1,7 @@
+<?php
+
+//all files from core folder must be included
+include("../app/core/app.php");
+include("../app/core/functions.php");
+include("../app/core/database.php");
+include("../app/core/controller.php");
---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+session_start();
+
+include("../app/init.php");
+
+
+echo ("Start");
