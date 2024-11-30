e8e08b1c22195f6435f0dc6e3abc051c193a3fc8
freetimezp <kraftukrnet@ukr.net>
2024-11-30 12:56:46 +0200

    03. php-mvc-03-e-commerce. MVC. App class. Grub URL.

--- app/init.php
+++ app/init.php
@@ -1,7 +1,7 @@
 <?php
 
 //all files from core folder must be included
-include("../app/core/app.php");
+include("../app/core/App.php");
 include("../app/core/functions.php");
 include("../app/core/database.php");
 include("../app/core/controller.php");
--- public/index.php
+++ public/index.php
@@ -5,4 +5,4 @@ session_start();
 include("../app/init.php");
 
 
-echo ("Start");
+$app = new App();
