9e1c913a846259d6b4e362f62b69264665cb82a7
freetimezp <kraftukrnet@ukr.net>
2024-11-30 13:33:53 +0200

    04. php-mvc-03-e-commerce. MVC. App class. Check controller & method. Route URL.

--- app/core/app.php
+++ app/core/app.php
@@ -4,18 +4,45 @@ class App
 {
     private $controller = "home";
     private $method = "index";
+    private $params;
 
     public function __construct()
     {
         $url = $this->parseURL();
+        //show($url);
 
-        show($url);
+        //check if file controller exist
+        if (file_exists("../app/controllers/" . strtolower($url[0]) . ".php")) {
+            $this->controller = strtolower($url[0]);
+            unset($url[0]);
+        }
+
+        //require controller
+        require "../app/controllers/" . $this->controller . ".php";
+        $this->controller = new $this->controller;
+
+        //check if method exist
+        if (isset($url[1])) {
+            $url[1] = strtolower($url[1]);
+
+            if (method_exists($this->controller, $url[1])) {
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+
+        $this->params = count($url) > 0 ? array_values($url) : ['home'];
+        //show($this->params);
+
+        call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
     private function parseURL()
     {
+        //grub url from browser
         $url = isset($_GET['url']) ? $_GET['url'] : "home";
 
+        //sinitize & trim URL
         return explode("/", filter_var(trim($url, "/"), FILTER_SANITIZE_URL));
     }
 }
