b9733ddf04921612b7faf3e2b010f3d7bb4ded78
freetimezp <kraftukrnet@ukr.net>
2024-12-01 19:40:44 +0200

    10. php-mvc-03-e-commerce. Signup.

---
+++ app/views/eshop/signup.php
@@ -0,0 +1,27 @@
+<?php $this->view("header", $data); ?>
+
+<section id="form"><!--form-->
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4 col-sm-offset-4">
+                <div class="signup-form"><!--sign up form-->
+                    <h2>New User Signup!</h2>
+                    <form action="#">
+                        <input type="text" placeholder="Name" />
+                        <input type="email" placeholder="Email Address" />
+                        <input type="password" placeholder="Password" />
+                        <button type="submit" class="btn btn-default">Signup</button>
+                    </form>
+                    <br>
+                    Already have an account?
+                    <a href="<?= ROOT ?>login">
+                        Login, please
+                    </a>
+                </div><!--/sign up form-->
+            </div>
+        </div>
+    </div>
+</section><!--/form-->
+
+
+<?php $this->view("footer", $data);  ?>
