68ebfb10fe745f64b08924c45da60c38f4a8a982
freetimezp <kraftukrnet@ukr.net>
2025-01-10 21:35:12 +0200

    79. php-mvc-03-e-commerce. Admin page. Users section. Correct data profile and orders.

--- app/controllers/profile.php
+++ app/controllers/profile.php
@@ -2,20 +2,32 @@
 
 class Profile extends Controller
 {
-    public function index()
+    public function index($url_address = null)
     {
         $data['page_title'] = "Profile";
 
         $user = $this->load_model('user');
         $order = $this->load_model('order');
 
-        $user_data = $user->check_login(true);
+        $orders = false;
 
+        $user_data = $user->check_login(true);
         if (!empty($user_data)) {
             $data['user_data'] = $user_data;
         }
 
-        $orders = $order->get_orders_by_user($user_data->url_address);
+        if ($url_address) {
+            $profile_data = $user->get_user($url_address);
+        } else {
+            $profile_data = $user_data;
+        }
+
+        if (is_object($profile_data)) {
+            $data['profile_data'] = $profile_data;
+            $orders = $order->get_orders_by_user($profile_data->url_address);
+        }
+
+
 
         if (is_array($orders)) {
 
--- app/views/eshop/profile.php
+++ app/views/eshop/profile.php
@@ -62,118 +62,125 @@
 </style>
 
 
-<section id="main-content" style="margin-left: 0;">
+<section id="main-content" class="container" style="margin: auto;">
     <section class="wrapper">
-        <div style="min-height: 400px;">
-            <div class="col-md-4 mb profile-block">
-                <div class="white-panel pn" style="box-shadow: 0 0 20px #aaa; border: solid thin #eee;">
-                    <div class="white-header">
-                        <h5>TOP USER</h5>
-                    </div>
-                    <p><img src="<?= ASSETS ?>admin/assets/img/ui-zac.jpg" class="img-circle" width="80"></p>
-                    <p class="name">
-                        <b><?= ucfirst($data['user_data']->name); ?></b>
-                    </p>
+        <?php if (isset($profile_data) && is_object($profile_data)): ?>
+            <div style="min-height: 400px;">
+                <div class="col-md-4 mb profile-block">
+                    <div class="white-panel pn" style="box-shadow: 0 0 20px #aaa; border: solid thin #eee;">
+                        <div class="white-header">
+                            <h5>TOP USER</h5>
+                        </div>
+                        <p><img src="<?= ASSETS ?>admin/assets/img/ui-zac.jpg" class="img-circle" width="80"></p>
+                        <p class="name">
+                            <b><?= ucfirst($profile_data->name); ?></b>
+                        </p>
 
-                    <div class="row">
-                        <div class="col-md-6">
-                            <p class="small mt">MEMBER SINCE</p>
-                            <p class="year">
-                                <?= date("jS M Y", strtotime($data['user_data']->date)); ?>
-                            </p>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <p class="small mt">MEMBER SINCE</p>
+                                <p class="year">
+                                    <?= date("jS M Y", strtotime($profile_data->date)); ?>
+                                </p>
+                            </div>
+                            <div class="col-md-6">
+                                <p class="small mt">TOTAL SPEND</p>
+                                <p class="price">$ 47,60</p>
+                            </div>
                         </div>
-                        <div class="col-md-6">
-                            <p class="small mt">TOTAL SPEND</p>
-                            <p class="price">$ 47,60</p>
+
+                        <hr style="opacity: 0.3; width: 90%; margin: 10px auto;">
+
+                        <div class="row">
+                            <div class="col-md-6">
+                                <p class="small mt" style="color: #d2795a; font-size: 15px; cursor: pointer;">
+                                    <i class="fa fa-edit" style="color: #d2795a;"></i> EDIT
+                                </p>
+                            </div>
+                            <div class="col-md-6">
+                                <p class="small mt" style="color: red; font-size: 15px; cursor: pointer;">
+                                    <i class="fa fa-trash"></i> DELETE
+                                </p>
+                            </div>
                         </div>
                     </div>
 
-                    <hr style="opacity: 0.3; width: 90%; margin: 10px auto;">
-
-                    <div class="row">
-                        <div class="col-md-6">
-                            <p class="small mt" style="color: #d2795a; font-size: 15px; cursor: pointer;">
-                                <i class="fa fa-edit" style="color: #d2795a;"></i> EDIT
-                            </p>
-                        </div>
-                        <div class="col-md-6">
-                            <p class="small mt" style="color: red; font-size: 15px; cursor: pointer;">
-                                <i class="fa fa-trash"></i> DELETE
-                            </p>
-                        </div>
-                    </div>
                 </div>
-            </div>
 
-            <?php if (is_array($orders)): ?>
-                <div class="col-md-8" style="box-shadow: 0 0 20px #aaa; border: solid thin #eee;">
-                    <h3>Orders:</h3>
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Date</th>
-                                <th>Total</th>
-                                <th>Delivery Address</th>
-                                <th>City/State</th>
-                                <th>Mobile</th>
-                                <th>...</th>
-                            </tr>
-                        </thead>
-                        <tbody onclick="show_details(event)">
-                            <?php foreach ($orders as $order): ?>
-                                <tr style="position: relative;">
-                                    <td><?= $order->id ?></td>
-                                    <td><?= date("jS M Y", strtotime($order->date)) ?></td>
-                                    <td>$<?= $order->total ?></td>
-                                    <td><?= $order->delivery_address ?></td>
-                                    <td><?= $order->country . "/" . $order->state ?></td>
-                                    <td><?= $order->mobile_phone ?></td>
-                                    <td>
-                                        <i class="fa fa-arrow-down"></i>
-                                        <div class="js-order-details details hide">
-                                            <span>close</span>
-                                            <?php if (isset($order->details) && is_array($order->details)): ?>
-                                                <h5>Order #<a style="color: blue;"><?= $order->id ?></a> details:</h5>
-                                                <table class="table" style="margin-top: 10px;">
-                                                    <thead>
-                                                        <tr>
-                                                            <th>Qty</th>
-                                                            <th>Description</th>
-                                                            <th>Amount</th>
-                                                            <th>Total</th>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody>
-                                                        <?php foreach ($order->details as $detail): ?>
-                                                            <tr>
-                                                                <td><?= $detail->qty ?></td>
-                                                                <td><?= $detail->description ?></td>
-                                                                <td>$<?= $detail->amount ?></td>
-                                                                <td>$<?= $detail->total ?></td>
-                                                            </tr>
-                                                        <?php endforeach; ?>
-                                                    </tbody>
-                                                </table>
-                                                <h5>Grand Total: $ <?= $order->grand_total ?> </h5>
-                                            <?php else: ?>
-                                                <div>
-                                                    No details for this order..
-                                                </div>
-                                            <?php endif; ?>
-                                        </div>
-                                    </td>
+                <?php if (isset($orders) && is_array($orders)): ?>
+                    <div class="col-md-8" style="box-shadow: 0 0 20px #aaa; border: solid thin #eee;">
+                        <h3>Orders:</h3>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Date</th>
+                                    <th>Total</th>
+                                    <th>Delivery Address</th>
+                                    <th>City/State</th>
+                                    <th>Mobile</th>
+                                    <th>...</th>
                                 </tr>
-                            <?php endforeach; ?>
-                        </tbody>
-                    </table>
-                </div>
-            <?php else: ?>
-                <div>
-                    Try to buy something...
-                </div>
-            <?php endif; ?>
-        </div>
+                            </thead>
+                            <tbody onclick="show_details(event)">
+                                <?php foreach ($orders as $order): ?>
+                                    <tr style="position: relative;">
+                                        <td><?= $order->id ?></td>
+                                        <td><?= date("jS M Y", strtotime($order->date)) ?></td>
+                                        <td>$<?= $order->total ?></td>
+                                        <td><?= $order->delivery_address ?></td>
+                                        <td><?= $order->country . "/" . $order->state ?></td>
+                                        <td><?= $order->mobile_phone ?></td>
+                                        <td>
+                                            <i class="fa fa-arrow-down"></i>
+                                            <div class="js-order-details details hide">
+                                                <span>close</span>
+                                                <?php if (isset($order->details) && is_array($order->details)): ?>
+                                                    <h5>Order #<a style="color: blue;"><?= $order->id ?></a> details:</h5>
+                                                    <table class="table" style="margin-top: 10px;">
+                                                        <thead>
+                                                            <tr>
+                                                                <th>Qty</th>
+                                                                <th>Description</th>
+                                                                <th>Amount</th>
+                                                                <th>Total</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <?php foreach ($order->details as $detail): ?>
+                                                                <tr>
+                                                                    <td><?= $detail->qty ?></td>
+                                                                    <td><?= $detail->description ?></td>
+                                                                    <td>$<?= $detail->amount ?></td>
+                                                                    <td>$<?= $detail->total ?></td>
+                                                                </tr>
+                                                            <?php endforeach; ?>
+                                                        </tbody>
+                                                    </table>
+                                                    <h5>Grand Total: $ <?= $order->grand_total ?> </h5>
+                                                <?php else: ?>
+                                                    <div>
+                                                        No details for this order..
+                                                    </div>
+                                                <?php endif; ?>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                <?php else: ?>
+
+                    <div>
+                        <h3>No orders yet..</h3>
+                        Try to buy something...
+                    </div>
+                <?php endif; ?>
+            </div>
+        <?php else: ?>
+            <h3 style="text-align:center;">Sorry, that profile could not be found</h3>
+        <?php endif; ?>
     </section>
 </section>
 
