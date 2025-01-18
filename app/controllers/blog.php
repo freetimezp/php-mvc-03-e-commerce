75a26d58eda5c0876d2c9fa9a8af43e6170b7fe9
freetimezp <kraftukrnet@ukr.net>
2025-01-18 21:18:11 +0200

    93. php-mvc-03-e-commerce. Blog Page.

---
+++ app/controllers/blog.php
@@ -0,0 +1,73 @@
+<?php
+
+class Blog extends Controller
+{
+    public function index()
+    {
+        //check if we use search
+        $search = false;
+        $find = "";
+        $show_search = true;
+
+        $DB = Database::newInstance();
+
+        if (isset($_GET['find'])) {
+            $search = true;
+            $find = addslashes($_GET['find']);
+        }
+
+        $data['page_title'] = "Home";
+
+        $user = $this->load_model('user');
+        $user_data = $user->check_login();
+
+        $image_class = $this->load_model('image');
+
+        if (!empty($user_data)) {
+            $data['user_data'] = $user_data;
+        }
+
+
+        $rows = false;
+        if ($search && !empty($find)) {
+            $arr['description'] = "%" . $find . "%";
+            $rows = $DB->read("SELECT * FROM products WHERE description LIKE :description ORDER BY id DESC", $arr);
+        } else {
+            $rows = $DB->read("SELECT * FROM products ORDER BY id DESC");
+        }
+
+        if ($rows) {
+            foreach ($rows as $key => $row) {
+                $rows[$key]->image = $image_class->get_thumb_post($rows[$key]->image);
+            }
+        }
+
+        //get all categories
+        $category = $this->load_model('category');
+        $categories = $category->get_all();
+        if ($categories) {
+            $data['categories'] = $categories;
+        }
+
+        //get all slider items
+        $slider = $this->load_model('slider');
+        $slider_rows = $slider->get_all();
+        if ($slider_rows) {
+            foreach ($slider_rows as $key => $row) {
+                $slider_rows[$key]->image = $image_class->get_thumb_post($slider_rows[$key]->image, 484, 441);
+            }
+
+            $data['slider_rows'] = $slider_rows;
+        }
+
+
+
+
+
+        $data['rows'] = $rows;
+        $data['show_search'] = $show_search;
+
+        //show($data);
+        $this->view("blog", $data);
+    }
+}
