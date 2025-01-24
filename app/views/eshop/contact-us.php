0712d8e5d6965078fbbc9238ffa3bc97e94cfa7a
freetimezp <kraftukrnet@ukr.net>
2025-01-24 22:42:28 +0200

    104. php-mvc-03-e-commerce. Contact US Page. Map.

--- app/views/eshop/contact-us.php
+++ app/views/eshop/contact-us.php
@@ -73,6 +73,14 @@
 				</div>
 			</div>
 		</div>
+
+
+		<h2 class="title"><strong>Find us</strong></h2>
+		<div class="row" style="margin: 20px 0;">
+			<div class="col-12">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d817324.2912233509!2d35.591399746834604!3d47.709520085766655!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sua!4v1737750946713!5m2!1sru!2sua" width="800" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+			</div>
+		</div>
 	</div>
 </div><!--/#contact-page-->
 
